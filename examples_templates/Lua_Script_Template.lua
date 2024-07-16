626af4df170c8e973416eb517614731545993da2
aliosgaming <84293623+aliosgaming@users.noreply.github.com>
2024-07-16 09:30:30 -0700

    Update Lua_Script_Template.lua

--- examples_templates/Lua_Script_Template.lua
+++ examples_templates/Lua_Script_Template.lua
@@ -8,18 +8,18 @@ function _OnInit()
 	if (GAME_ID == 0xF266B00B or GAME_ID == 0xFAF99301) and ENGINE_TYPE == "ENGINE" then --PCSX2
 		canExecute = true
 		vars = require("KH2Emulator")
-		print('KH2 Library Example - Emulator')
+		print('Emulator')
 	elseif GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then --PC
 		canExecute = true
 		if ReadByte(0x566A8E) == 0xFF then --EGS Global
 			vars = require("KH2EpicGamesGlobal")
-			ConsolePrint('KH2 Library Example - Epic Games Global')
+			ConsolePrint('Epic Games Global')
 		elseif ReadByte(0x56668E) == 0xFF then --Steam Global
 			vars = require("KH2SteamGlobal")
-			ConsolePrint('KH2 Library Example - Steam Global')
+			ConsolePrint('Steam Global')
 		elseif ReadByte(0x56640E) == 0xFF then --Steam JP
 			vars = require("KH2SteamJP")
-			ConsolePrint('KH2 Library Example - Steam JP')
+			ConsolePrint('Steam JP')
 		else
 			canExecute = false
 			ConsolePrint('KH2 not detected, not running script')
