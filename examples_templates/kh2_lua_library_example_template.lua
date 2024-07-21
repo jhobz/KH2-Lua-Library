d8094e177330c7b4343e8b688b0acb6943216956
aliosgaming <84293623+aliosgaming@users.noreply.github.com>
2024-07-21 00:34:15 -0700

    Update kh2_lua_library_example_template.lua

--- examples_templates/kh2_lua_library_example_template.lua
+++ examples_templates/kh2_lua_library_example_template.lua
@@ -25,8 +25,7 @@ function _OnInit()
 			canExecute = false
 			ConsolePrint('KH2 not detected, not running script')
 		end
-	else
-		ConsolePrint('KH2 not detected, not running script')
+	else print('KH2 not detected, not running script')
 	end
 end
 
