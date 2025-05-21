0bce1d740b8cc5010822577a9226c1ae5f6daf4f
Jeff Hobson <jeffrey296@gmail.com>
2025-05-20 23:31:25 -0400

    Remove globals from example script and update code style
    Globals should be avoided in favor of locals whenever possible.

--- examples_templates/kh2_lua_library_example_template.lua
+++ examples_templates/kh2_lua_library_example_template.lua
@@ -1,29 +1,31 @@
-LUAGUI_NAME = ""
-LUAGUI_AUTH = ""
-LUAGUI_DESC = ""
+LUAGUI_NAME = ''
+LUAGUI_AUTH = ''
+LUAGUI_DESC = ''
+
+local kh2lib_status, kh2lib, can_execute
 
 function _OnInit()
-    kh2libstatus, kh2lib = pcall(require, "kh2lib")
-    if not kh2libstatus then
-        print("ERROR (My Script): KH2-Lua-Library mod is not installed")
-        CanExecute = false
+    kh2lib_status, kh2lib = pcall(require, 'kh2lib')
+    if not kh2lib_status then
+        ConsolePrint('KH2-Lua-Library mod is not installed', 3)
+        can_execute = false
         return
     end
 
-    -- Log("My Script 1.0.5")
+    Log('My Script 1.0.0')
     RequireKH2LibraryVersion(1)
     -- RequirePCGameVersion()
 
-    CanExecute = kh2lib.CanExecute
-    if not CanExecute then
+    can_execute = kh2lib.CanExecute
+    if not can_execute then
         return
     end
 
-    -- Now = kh2lib.Now
+    -- (your code here)
 end
 
 function _OnFrame()
-    if not CanExecute then
+    if not can_execute then
         return
     end
 
