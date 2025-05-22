f93f01601a6c3e131f66ac04622b61c4414a4343
Jeff Hobson <jeffrey296@gmail.com>
2025-05-22 12:13:45 -0400

    Use locals in LuaLibrary.lua

--- LuaLibrary.lua
+++ LuaLibrary.lua
@@ -1,20 +1,22 @@
+local kh2lib_status, kh2lib, can_execute
+
 function _OnInit()
-    kh2libstatus, kh2lib = pcall(require, "kh2lib")
-    if not kh2libstatus then
-        print("ERROR: KH2-Lua-Library mod is not installed")
-        CanExecute = false
+    kh2lib_status, kh2lib = pcall(require, 'kh2lib')
+    if not kh2lib_status then
+        print('ERROR: KH2-Lua-Library mod is not installed')
+        can_execute = false
         return
     end
 
-    CanExecute = kh2lib.CanExecute
-    if not CanExecute then
+    can_execute = kh2lib.CanExecute
+    if not can_execute then
         return
     end
 
-    Log("#####################################################")
-    Log("# KH2 Lua Library, version " .. KH2_LIBRARY_DISPLAY_VERSION)
-    Log("# Using addresses for " .. kh2lib.GameVersionString)
-    Log("#####################################################")
+    Log('#####################################################')
+    Log('# KH2 Lua Library, version ' .. KH2_LIBRARY_DISPLAY_VERSION)
+    Log('# Using addresses for ' .. kh2lib.GameVersionString)
+    Log('#####################################################')
 end
 
 function _OnFrame()
